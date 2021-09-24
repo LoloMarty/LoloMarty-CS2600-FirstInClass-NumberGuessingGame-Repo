74ead218d2d3d28d4a136b4340dbaa3791038ffa
Marvin <=>
2021-09-24 15:35:59 -0700

    Contains the outline of the code written in comments. There is no code to be found here.

---
+++ NumberGuessingGame.c
@@ -0,0 +1,50 @@
+//define a maxNumber int variable 
+//define a masterMaxNumber int variable
+//define a randInt variable -> [STICK THIS IN SHOW MENU CONDITIONAL] 
+//define showMenu boolean
+
+//define a currentMenuVal int variable, checks what the user has chosen based on 
+//the 3 possible menu options 
+
+//define a guess int variable, holds the user's guessed number 
+
+//              [IF OPTION 1 IS SELECTED]
+//prompt the user to enter their guessing number 
+
+//  ** IF GUESS == 'q' then:
+//      -showMenu == true
+//      -CONTINUE (the statement like break)
+
+//check if guess == randInt: if so then:
+//      -print congradulations
+//      -showMenu = true
+//      -Loop
+
+//if guess != randInt && guess < randInt:
+//      -print that the guess is too low 
+//      -keep randInt the same
+//      -keep currentMenuVal the same
+
+//if guess != randInt && guess > randInt:
+//      -print that the guess is too high 
+//      -keep randInt the same
+//      -keep currentMenuVal the same
+
+
+
+//              [IF OPTION 2 IS SELECTED]
+// print out an enter number statement
+// prompt the user for their number and store it in guess
+// evaluate if that number is > masterMaxNumber and currentMenuVal = true
+// if not then pass it to maxNumber 
+// if it is then:
+//      -print error
+//      -showMenu = false
+//      -currentMenuVal stays the same 
+
+
+
+//              [IF OPTION 3 IS SELECTED]
+// set currentMenuVal to 3 and call CONTINUE (let the while loop break itself)
+
+//use a while loop to determine if the program should end 
